ba8a03230dcc5410a459a25a9839ac8fa9e8891d
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-02 21:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ let array = [];
 }
 
 // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
-// alert(stringChop(str, size));
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));
